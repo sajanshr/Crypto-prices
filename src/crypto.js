c5e0f2394e383f1186c3a9b599018d190d820497
Sajan Shrestha <sajnshrestha021@gmail.com>
2024-10-12 20:07:12 -0500

    Initialized crypto repo with some css

---
+++ src/crypto.js
@@ -0,0 +1,11 @@
+import './Crypto.css';
+
+function Crypto() {
+  return (
+    <div className="Crypto">
+  
+    </div>
+  );
+}
+
+export default Crypto;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Crypto from './crypto';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Crypto />
   </React.StrictMode>
 );
 
